1718da20a24cd9b1bc614bef498005fe4e5fda40
Derrick Mwiti <mwitiderrick@gmail.com>
2018-04-06 21:50:30 +0000

    Scope Fixed

--- index.js
+++ index.js
@@ -3,9 +3,12 @@
 angular.module('eits', [])
   .controller('EitController', function EitController() {
   this.cy = new Date();
+  this.fname;
+  this.lname;
+  this.dob;
   
     this.getAge = function getAge(dob) {
-      this.mil = this.cy - dob;
+      this.mil = this.cy - this.dob;
       this.years = Math.floor(this.mil / 3.154e+10);
       this.months = Math.floor((this.mil % 3.154e+10) / 2.628e+9);
      
@@ -14,9 +17,9 @@ angular.module('eits', [])
 
     };
     
-    this.getFullName = function getFullName(fname,){
+    this.getFullName = function getFullName(fname,lname){
       
-      return fname + lname;
+      return this.fname + ' '+ this.lname;
 
     };
    
