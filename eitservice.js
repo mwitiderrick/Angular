bdeb55042a3580ce611c133f043d3a6c0ed6a9a4
Derrick Mwiti <mwitiderrick@gmail.com>
2018-04-11 12:53:58 +0000

    More fuctions

--- eitservice.js
+++ eitservice.js
@@ -1,16 +1,39 @@
 angular.module('EIT', [])
 .factory('EitService', function() {
-  
-  var eits = [];
+	var currentEit = {};
+	var eits = [];
+	var counter = 0;
 
-  var addEit = function(firstName, lastName) {
-      eits.push({firstName,lastName});
-      console.log(eits)
-  };
+	var addEit = function(firstName, lastName,dob,gender) {
 
-  return {
-    addEit: addEit,
-    eits : eits
-  };
+	eits.push({id:counter,firstName,lastName,dob,gender});
+	counter = counter + 1;
+	console.log(eits)
 
+	};
+
+	var getEit = function(id){
+	var e = eits.find(eit => eit.id === id);
+	setCurrentEit(e);
+	return e;
+	
+	};
+
+	var setCurrentEit = function(eit){
+		currentEit = eit;
+	};
+
+	var getCurrentEit = function(){
+		return currentEit;
+	};
+
+	return {
+	setCurrentEit:setCurrentEit,
+	getCurrentEit:getCurrentEit,
+	addEit: addEit,
+	eits : eits,
+	getEit:getEit,
+	currentEit:currentEit
+
+	};
 });
--- index.js
+++ index.js
@@ -1,38 +1,52 @@
-(function(angular) {
-  'use strict';
+
 angular.module('eits', ['EIT'])
-  .controller('EitController', ['EitService',function EitController(EitService) {
-  this.cy = new Date();
-  this.fname;
-  this.lname;
-  this.dob;
-  
+  .controller('EitController', ['EitService',
+  function EitController(EitService) {
+    this.cy = new Date();
     this.getAge = function getAge(dob) {
-      this.mil = this.cy - this.dob;
-      this.years = Math.floor(this.mil / 3.154e+10);
-      this.months = Math.floor((this.mil % 3.154e+10) / 2.628e+9);
+    this.mil = this.cy - this.dob;
+    this.years = Math.floor(this.mil / 3.154e+10);
+    this.months = Math.floor((this.mil % 3.154e+10) / 2.628e+9);
+    return this.years + 'year(s)  ' + this.months + 'months(s)';
+      };
+      
+      this.getFullName = function getFullName(fname,lname){
+        return this.fname + ' '+ this.lname;
+
+      };
      
 
-      return this.years + 'yea(rs)  ' + this.months + 'months(s)';
+    }]).controller('ListController',['EitService',
+      function ListController(EitService) {
+        this.getEits = function getEits(){
+
+      return EitService.eits;
+
+      };
+
+      this.showEIt = function showEit(eit){
+        return EitService.showEIt;
+      };
+
+}]).controller('DetailsController',['EitService',
+  function DetailsController(EitService) { 
+    this.getEit = function(id){
+      console.log(id);
+      this.eit =  EitService.getEit(id);
 
     };
-    
-    this.getFullName = function getFullName(fname,lname){
-      
-      return this.fname + ' '+ this.lname;
 
-    };
-   
-   this.addEit = function addEit(){
-     return EitService.addEit(this.fname, this.lname);
-   };
+    this.eit =  function(){
+      return EitService.getCurrentEit();
+    }
+}]).controller('AddEditController',['EitService',
+  function AddEditController(EitService) { 
+    this.fname;
+    this.lname;
+    this.dob;
+    this.addEdit = function addEit(){
+       return EitService.addEit(this.fname, this.lname, this.dob, this.gender);
+     };
+}]);
 
-  this.getEits = function getEits(){
-
-    return EitService.eits;
-
-    };
-
-  }]);
-})(window.angular);
 
